1a6db614ff5bfcf926f58f55006a67812b26c4ec
ourique787 <lucastrajano93@gmail.com>
2024-11-22 18:02:46 -0300

    parcialmente completo aula2

---
+++ resources/views/pokemon/index.blade.php
@@ -0,0 +1,31 @@
+@extends('layouts.base')
+
+
+@section('content')
+
+    @foreach($pokemon as $entity)
+    
+
+        <div class="w-full max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+           
+            <div class="flex flex-col items-center pb-10">
+                <img class="w-24 h-24 mb-3 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image"/>
+                <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{{ $entity->name }}</h5>
+                <span class="text-sm text-gray-500 dark:text-gray-400">{{ $entity->type }}</span>
+                <span class="text-sm text-gray-500 dark:text-gray-400">{{ $entity->power }}</span>
+                <div class="flex mt-4 md:mt-6">
+                    <a href="{{ url('pokemon/'.$entity->id.'/edit') }}" class="inline-flex items-center px-4 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Edit</a>
+                    <form action="{{ url('pokemon/'.$entity->id) }}" method="POST">
+                        @csrf
+                        @method('DELETE')
+                        <button type="submit" class="py-2 px-4 ms-2 text-sm font-medium text-gray-900 focus:outline-none bg-white rounded-lg border border-gray-200 hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-4 focus:ring-gray-100 dark:focus:ring-gray-700 dark:bg-gray-800 dark:text-gray-400 dark:border-gray-600 dark:hover:text-white dark:hover:bg-gray-700">Delete</button>
+                    </form>
+                </div>
+        </div>
+
+
+
+    @endforeach 
+
+@endsection
+
